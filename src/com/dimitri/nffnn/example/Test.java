d9308db963d3914d7c698e05d3c2f3761b6a7b6b
dstallenberg <33372913+dstallenberg@users.noreply.github.com>
2017-11-21 15:34:07 +0100

    Added some Graphical elements like a grapher
    worked out the test package to include gui

--- src/com/dimitri/nffnn/example/Test.java
+++ src/com/dimitri/nffnn/example/Test.java
@@ -2,42 +2,36 @@ package com.dimitri.nffnn.example;
 
 import com.dimitri.nffnn.neural.Net;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Random;
 
 public class Test {
 
+    private static Unit unit;
+
+    private static MultiThread update;
+    private static MultiThread render;
+
+    public static int delay = 0;
+
     public static void main(String... args){
+        unit = new Unit();
+        update = new MultiThread(unit, "Update");
+        render = new MultiThread(unit, "Render");
+        update.start();
+        render.start();
+    }
+
+    public static void quit(Net net) {
+        update.Running = false;
+        render.Running = false;
+        net.setFilePath("weights.txt");
         try {
-            Net net = new Net(new int[]{2,5,2});
-            Random random = new Random();
-            double[] input = new double[2];
-            double[] target = new double[2];
-
-            for (int i = 0; i < 1000; i++) {
-                input[0] = random.nextInt(2);
-                input[1] = random.nextInt(2);
-
-                if(input[0] == input[1]){
-                    target[0] = 1;
-                    target[1] = 0;
-                }else{
-                    target[0] = 0;
-                    target[1] = 1;
-                }
-
-                net.feedForward(input);
-                net.backPropogation(target);
-                double[] output = net.getOutput();
-                System.out.println("Input: " + input[0] + ", " + input[1]);
-                System.out.println("Target: " + target[0] + ", " + target[1]);
-                System.out.println("Output: " + output[0] + ", " + output[1]);
-                System.out.println(net.getRecentAverageError());
-            }
-            net.setFilePath("weights.txt");
             net.getIo().Write(net);
-        } catch (IOException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        System.exit(0);
     }
 }
